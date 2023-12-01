2b60c1fbf6dd4b33611241a14daa6bd5a2795fd5
Cedric Daunke <daunke40@gmail.com>
2023-12-01 15:59:27 +0100

    added add and delete function

--- script.js
+++ script.js
@@ -2,41 +2,62 @@ let savedNotesTitle = [];
 let savedNotesText = [];
 let deletedNotesTitle = [];
 let deletedNotesText = [];
+loadFromLocalStorage();
 
 function init() {
-  console.log("loaded");
-  let finishedNote = document.getElementById("finishedNote");
-  finishedNote.innerHTML += renderNewNote();
+  let finishedNote = document.getElementById("finishedNotesContainer");
+  finishedNote.innerHTML = "";
+  renderNotes();
 }
 
 function saveNote() {
-  console.log("saved");
   let resetForm = document.getElementById("NoteForm");
   let valueTitle = document.getElementById("noteTitle").value;
   let valueNoteText = document.getElementById("NoteText").value;
   savedNotesTitle.push(valueTitle);
   savedNotesText.push(valueNoteText);
+  saveInLocalStorage();
+  loadFromLocalStorage();
+  init();
   resetForm.reset();
 }
 
-function saveInLocalStorage() {
+function deleteNotes(i) {
+  savedNotesTitle.splice(i,1);
+  savedNotesText.splice(i,1);
+  saveInLocalStorage();
+  init();
+}
 
+function saveInLocalStorage() {
+  let NoteTitle = JSON.stringify(savedNotesTitle);
+  localStorage.setItem("savedTitle", NoteTitle);
+  let NoteText = JSON.stringify(savedNotesText);
+  localStorage.setItem("savedText", NoteText);
 }
 
 function loadFromLocalStorage() {
-
+  let loadTitle = localStorage.getItem("savedTitle");
+  let loadText = localStorage.getItem("savedText");
+  if (loadTitle && loadText) {
+    savedNotesTitle = JSON.parse(loadTitle);
+    savedNotesText = JSON.parse(loadText);
+  }
 }
 
-function renderNewNote() {
-    let finishedNote = document.getElementById('finishedNote');
-    finishedNote.innerHTML = '';
-
-    for (let i = 0; i < savedNotesTitle.length; i++) {
-        finishedNote.innerHTML += `
-        <div id="finishedNote">
-                <h1>${savedNotesTitle}</h1>
-                <p>${savedNotesText}</p>
+function renderNotes() {
+  for (let i = 0; i < savedNotesTitle.length; i++) {
+    let finishedNote = document.getElementById("finishedNotesContainer");
+    let NoteTitle = savedNotesTitle[i];
+    let NoteText = savedNotesText[i];
+    finishedNote.innerHTML += `
+    <div id="finishedNote">
+            <div class="newNoteHeader">
+            <h1>${NoteTitle}</h1>
+            <img src="./img/icons/trash-solid.svg" class="smallTrashIcon" onclick="deleteNotes(${i})">
             </div>
-        `;  
-    }
+            <p>${NoteText}</p>
+        </div>
+    `;
+  }
 }
