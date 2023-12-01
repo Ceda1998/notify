c76378670309af91795162cff90349ce979024f5
Cedric Daunke <daunke40@gmail.com>
2023-12-01 17:53:58 +0100

    added delete function and trashcan

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function saveNote() {
 }
 
 function deleteNotes(i) {
+  deletedNotesTitle.push(savedNotesTitle[i]);
+  deletedNotesText.push(savedNotesText[i]);
   savedNotesTitle.splice(i,1);
   savedNotesText.splice(i,1);
   saveInLocalStorage();
@@ -34,14 +36,33 @@ function saveInLocalStorage() {
   localStorage.setItem("savedTitle", NoteTitle);
   let NoteText = JSON.stringify(savedNotesText);
   localStorage.setItem("savedText", NoteText);
+  let trashNoteTitle = JSON.stringify(deletedNotesTitle);
+  localStorage.setItem("deletedTitle",trashNoteTitle);
+  let trashNoteText = JSON.stringify(deletedNotesText);
+  localStorage.setItem("deletedText", trashNoteText)
 }
 
 function loadFromLocalStorage() {
   let loadTitle = localStorage.getItem("savedTitle");
   let loadText = localStorage.getItem("savedText");
+  let loadDeletedTitle = localStorage.getItem("deletedTitle")
+  let loadDeletedText = localStorage.getItem("deletedText")
   if (loadTitle && loadText) {
     savedNotesTitle = JSON.parse(loadTitle);
     savedNotesText = JSON.parse(loadText);
+  }
+  if (loadDeletedTitle && loadDeletedText) {
+    deletedNotesTitle = JSON.parse(loadDeletedTitle);
+    deletedNotesText = JSON.parse(loadDeletedText);
+  }
+}
+
+function toggleTrashCan() {
+  let trashCan = document.getElementById('trascan');
+  if (trashCan.style.display == 'none') {
+    trashCan.style.display = 'block';
+  } else {
+    trashCan.style.display = 'none';
   }
 }
 
